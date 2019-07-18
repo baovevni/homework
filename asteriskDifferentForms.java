8d6f77ef9595ce732a3c2aa3c1c29c8a9ac7c347
baovevni <Barducov@MBP-Barducov.mshome.net>
2019-07-18 15:15:11 +0300

    added DifferentForms task

---
+++ asteriskDifferentForms.java
@@ -0,0 +1,15 @@
+public class asteriskDifferentForms{
+	public static void main(String args[]){
+	char a = '*';
+	String w = " ";
+	System.out.println(w + w + a + a + a + a + a + a + a + a + a + w + w + w + w + w + w + a + a + a + w + w + w + w + w + w + w + a + w + w + w + w + w + w + w + w + a + w + w + w + w + w);
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + a + w + w + w + w + a + a + a + w + w + w + w + w + w + a + w + a + w + w + w + w);
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + a + w + w + w + w + w + w + w + a + w + w + a + a + a + a + a + w + w + w + w + a + w + w + w + a + w + w + w);
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + a + w + w + w + w + w + a + w + w); 
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + w + w + a + w); 
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + a + w + w + w + w + w + a + w + w);
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + w + a + w + w + w + a + w + w + w);
+	System.out.println(w + w + a + w + w + w + w + w + w + w + a + w + w + w + w + a + w + w + w + w + w + a + w + w + w + w + w + a + w + w + w + w + w + w + w + a + w + a + w + w + w + w);
+	System.out.println(w + w + a + a + a + a + a + a + a + a + a + w + w + w + w + w + w + a + a + a + w + w + w + w + w + w + w + a + w + w + w + w + w + w + w + w + a + w + w + w + w + w);
+	}
+} 
