350b08a57dc8fdcb4d4aa52540e94d3eda8b35dd
baovevni <Barducov@MBP-Barducov.mshome.net>
2019-07-18 14:26:16 +0300

    completed task 2.27

---
+++ asterisksTwoTwoSeven.java
@@ -0,0 +1,13 @@
+public class asterisksTwoTwoSeven{
+	public static void main(String args[]){
+	char s = '*';
+	System.out.println(s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  ");
+	System.out.println("  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s);
+	System.out.println(s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  ");
+	System.out.println("  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s);
+	System.out.println(s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  ");
+	System.out.println("  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s);
+	System.out.println(s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  ");
+	System.out.println("  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s + "  " + s);
+	}
+}
