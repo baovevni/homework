36adcfbcd8663b9095d001fc07ddb25f741277d9
baovevni <evghenii.barducov@gmail.com>
2019-08-02 13:31:19 +0300

    Changed logic in some tasks

--- exercise_white/src/ExerciseWhite_1/Bmi.java
+++ exercise_white/src/ExerciseWhite_1/Bmi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Bmi {
     public static final int inchesInFeet = 12;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many feets you are?");
@@ -12,13 +13,12 @@ public class Bmi {
         int heightInches = scanner.nextInt();
         System.out.println("What is your weight in pounds?");
         int weight = scanner.nextInt();
-        int heightInInches = (heightFoots*inchesInFeet + (heightInches*inchesInFeet)/inchesInFeet);
-        double heightInMeters = heightInInches*0.0254;
+        int heightInInches = (heightFoots * inchesInFeet + (heightInches * inchesInFeet) / inchesInFeet);
+        double heightInMeters = heightInInches * 0.0254;
         double weightKg = weight / 2.2;
-        double bmi = weightKg / (heightInMeters*heightInMeters);
+        double bmi = weightKg / (heightInMeters * heightInMeters);
         System.out.println("Your BMI is " + bmi);
 
-        double result = Degrees.convertToDecimalDegrees(32, 65, 678);
     }
 }
 
